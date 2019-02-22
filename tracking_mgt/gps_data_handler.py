51d2748f4a42a887b346db0bc6da56d699662ab5
ryo <ryo@ryolaptop.ryosuwito.com>
2019-02-22 14:52:36 +0700

    gps_data_handler

--- tracking_mgt/gps_data_handler.py
+++ tracking_mgt/gps_data_handler.py
@@ -178,7 +178,6 @@ else:
 max_day = 0
 day = 30
 last_day = day - 1
-now = datetime.now()
 while day > max_day:
     res = gps_handler.get_all_vehicle()
     if res['response'] == 'OK':
@@ -186,6 +185,7 @@ while day > max_day:
     else:
         print("Get all vehicles Failed")
         exit()
+    now = datetime.now()
     date_start = now - timedelta(days = day)
     date_now = now - timedelta(days = last_day)
     date_start = date_start.strftime('%Y-%m-%d %H:%M:%S')

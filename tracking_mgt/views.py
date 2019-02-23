b0cc614d285b870677e02882c270008cc17ea154
ryo <ryo@ryolaptop.ryosuwito.com>
2019-02-23 10:48:29 +0700

    change get all license

--- tracking_mgt/views.py
+++ tracking_mgt/views.py
@@ -45,16 +45,17 @@ def get_by_license(license_no, **kwargs):
 
 	if end_date:
 		if not start_date:
-			gps = GpsData.objects.filter(license_no=license_no.upper(), created_date__lte=end_date).values('data').iterator()
+			gps = GpsData.objects.filter(license_no=license_no.upper(), created_date__lte=end_date).iterator()
 		else:
-			gps = GpsData.objects.filter(license_no=license_no.upper(), created_date__gte=start_date, created_date__lte=end_date).values('data').iterator()
+			gps = GpsData.objects.filter(license_no=license_no.upper(), created_date__gte=start_date, created_date__lte=end_date).iterator()
 	elif start_date:
-		gps = GpsData.objects.filter(license_no=license_no.upper(), created_date__gte=start_date).values('data').iterator()
+		gps = GpsData.objects.filter(license_no=license_no.upper(), created_date__gte=start_date).iterator()
 	else:
-		gps = GpsData.objects.filter(license_no=license_no.upper()).values('data').iterator()
+		gps = GpsData.objects.filter(license_no=license_no.upper()).iterator()
 
 	data = [{'lat':g.data['latitude'],
-			'lng':g.data['longitude']} for g in gps]
+			'lng':g.data['longitude'],
+			'mileage':g.data['mileage']} for g in gps]
 			
 	return {
 			'license_no':license_no,

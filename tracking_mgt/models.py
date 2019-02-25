dd68135576bd1c8c5be1e89ec0abd80cd182423e
ryo <ryo@ryolaptop.ryosuwito.com>
2019-02-25 14:41:20 +0700

    adding city field to lastlocation

--- tracking_mgt/models.py
+++ tracking_mgt/models.py
@@ -1,50 +1,23 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+class GpsDailyReport(models.Model):
+    created_date = models.DateTimeField(auto_now=False, db_index=True, blank=True, null=True)
+    mileage = models.IntegerField()
+    viewer = models.IntegerField()
+    def __str__(self):
+        return "%s / %s / %s"%(self.created_date, self.mileage, self.viewer)
+
 class GpsData(models.Model):
     license_no = models.CharField(max_length=15, db_index=True)
     created_date = models.DateTimeField(auto_now=False, db_index=True, blank=True, null=True)
     timestamp = models.CharField(max_length=60, db_index=True)
     data = JSONField(null=True)
-    """ 
-    gps_state = models.CharField(max_length=30, null=True, blank=True)
-    gps = models.IntegerField()
-    time = models.IntegerField()
-    username = models.CharField(max_length=30)
-    time_format = models.CharField(max_length=30)
-    terminal = models.CharField(max_length=15)
-    owner_telp = models.CharField(max_length=20, null=True, blank=True)
-    owner_name = models.CharField(max_length=50, null=True, blank=True)
-    gps_type = models.CharField(max_length=30, null=True, blank=True)
-    longitude = models.IntegerField()
-    latitude = models.IntegerField()
-    address = models.CharField(max_length=255, null=True, blank=True)
-    speed = models.IntegerField()
-    direction = models.IntegerField()
-    mileage = models.IntegerField()
-    alarm_state = models.IntegerField(null=True, blank=True)
-    car_state = models.IntegerField(null=True, blank=True)
-    icon_id = models.IntegerField(null=True, blank=True)
-    gsm = models.CharField(max_length=20, null=True, blank=True)
-    status = models.CharField(max_length=15, null=True, blank=True)
-    status_engine = models.CharField(max_length=15, null=True, blank=True)
-    exp_date = models.DateField(auto_now=False, null=True, blank=True)
-    status_exp = models.CharField(max_length=15, null=True, blank=True)
-    vehicle_state = models.CharField(max_length=15, null=True, blank=True)
-    vehicle_type = models.CharField(max_length=15, null=True, blank=True)
-    kelurahan = models.CharField(max_length=50)
-    kecamatan = models.CharField(max_length=30)
-    kota = models.CharField(max_length=30)
-    provisi = models.CharField(max_length=30)
-    user_login = models.CharField(max_length=15)
-    password = models.CharField(max_length=15)
-    column = models.IntegerField()
-    """
-    
     def __str__(self):
         return self.license_no
 
 class ImpressionGpsData(models.Model):
+    city = models.CharField(max_length=255, db_index=True, null=True)
     address = models.CharField(max_length=500, db_index=True, null=True)
     license_no = models.CharField(max_length=15, db_index=True)
     latitude = models.CharField(blank=True, null=True, max_length=55, db_index=True)
@@ -59,6 +32,7 @@ class ImpressionGpsData(models.Model):
 
 
 class LastLocation(models.Model):
+    city = models.CharField(max_length=255, db_index=True, null=True)
     address = models.CharField(max_length=500, db_index=True, null=True)
     license_no = models.CharField(max_length=15, db_index=True)
     latitude = models.CharField(blank=True, null=True, max_length=55, db_index=True)

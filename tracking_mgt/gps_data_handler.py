3ca9923e693cfc5c0c1952e16addcf0be2d4e77c
ryo <ryo@ryolaptop.ryosuwito.com>
2019-02-27 11:06:11 +0700

    gps every 2 hours

--- tracking_mgt/gps_data_handler.py
+++ tracking_mgt/gps_data_handler.py
@@ -314,7 +314,7 @@ while True:
     day = 0
     last_day = day - 1
 
-    sleep_time = 120
+    sleep_time = 10
     st = 0
     print("sleeping for %s minutes"%(sleep_time))
     while st <= sleep_time:

ce08d82d4d3ed914c33b5be2e108cd77a2e7cf09
ryo <ryo@ryolaptop.ryosuwito.com>
2019-03-01 18:18:33 +0700

    add daily mileage report

--- tracking_mgt/admin.py
+++ tracking_mgt/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import LastLocation, GpsData
+from .models import LastLocation, GpsData, GpsDailyReport
 
 class LastLocationAdmin(admin.ModelAdmin):
     model = LastLocation
 class GpsDataAdmin(admin.ModelAdmin):
     model = LastLocation
+class GpsDailyReportAdmin(admin.ModelAdmin):
+    model = GpsDailyReport
     
 admin.site.register(LastLocation, LastLocationAdmin)
+admin.site.register(GpsDailyReport, GpsDailyReportAdmin)
 admin.site.register(GpsData, GpsDataAdmin)

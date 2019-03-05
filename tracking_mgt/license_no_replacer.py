aee2796d218fa934d0b1aa1f21c4c7ec719e1696
ryo <ryo@ryolaptop.ryosuwito.com>
2019-03-05 13:39:43 +0700

    add checking duplicate function

--- tracking_mgt/license_no_replacer.py
+++ tracking_mgt/license_no_replacer.py
@@ -22,3 +22,33 @@ def replace():
 		location.license_no = l
 		location.save()
 		print("Gps : %s of %s : %s"%(idx, data_len, l))
+
+def find_duplicate():
+	data = GpsData.objects.all().exclude(campaign_name='marugame').exclude(campaign_name='phd').iterator()
+	for idx, d in enumerate(data):
+		try:
+			ob = GpsData.objects.get(license_no=d.license_no, 
+				timestamp=d.timestamp)
+			if 'phd' in ob.campaign_name:
+				c_name = 'phd'
+			elif 'marugame' in ob.campaign_name:
+				c_name = 'marugame'
+			else:
+				print(ob.campaign_name)
+				print('+++++++++++++++++++++++++++')
+				continue
+			try:
+				ex = GpsData.objects.get(license_no=d.license_no, 
+				timestamp=d.timestamp, campaign_name = c_name)
+				print('DUPLICATE FOUND !!!')
+				print(' - '.join(ex.license_no, ob.license_no))
+				print(' - '.join(ex.timestamp, ob.timestamp))
+				print(ob.data)
+				print(ex.data)
+				ob.delete()
+			except:
+				print('EX FOUND - DUPLICATE NOT FOUND')
+				ob.campaign_name = c_name
+		except:
+			print('DUPLICATE NOT FOUND')
+			ob.campaign_name = c_name

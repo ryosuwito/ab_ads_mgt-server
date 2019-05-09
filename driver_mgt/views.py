41eda43a1017357e8528805dca5428ac1c374886
ryosuwito <ryosuwito@gmail.com>
2019-05-09 16:15:13 +0700

    adding bukti tayang

--- driver_mgt/views.py
+++ driver_mgt/views.py
@@ -158,9 +158,7 @@ def driver_login_view(request, *args, **kwargs):
 
 @csrf_exempt
 def driver_upload_bukti_tayang(request, license_no, *args, **kwargs):
-    files = request.FILES.get("file")
-    print(files)
+    files = request.FILES['file']
     token = secrets.token_urlsafe(25)
     bukti_tayang = BuktiTayang.objects.create(license_no=license_no, photo=files, created_date=datetime.now())
-    print(len(files))
     return JsonResponse({"plat":license_no, "token":token})

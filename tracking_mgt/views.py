54c151939e8ed2734565339c1f9886c0d8d223c3
ryosuwito <ryosuwito@gmail.com>
2019-05-15 13:14:05 +0700

    change tracking_mgt_view

--- tracking_mgt/views.py
+++ tracking_mgt/views.py
@@ -411,6 +411,8 @@ def save_gps_data(request, license_no, *args, **kwargs):
 	campaign = "".join([c for c in campaign if c.isalnum()])
 	license_no = license_no.lower()
 	license_no = "".join([l for l in license_no if l.isalnum()])
+	if campaign != settings.CAMPAIGN_NAME and campaign != "":
+		return HttpResponseRedirect("http://"+campaign+".abplusscar.com/gps/save/"+license_no+"/?lat="+lat+"&lng="+lng+"&cmp="+campaign)
 	try:
 		last_gps = LastDummyGps.objects.get(license_no=license_no)
 	except:

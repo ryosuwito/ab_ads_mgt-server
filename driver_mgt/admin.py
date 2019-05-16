ff85311d3876cbaecc53362967f96889bda4a281
ryosuwito <ryosuwito@gmail.com>
2019-05-16 10:20:38 +0700

    add annotate phone

--- driver_mgt/admin.py
+++ driver_mgt/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Driver
+from .models import Driver, BuktiTayang
 
 class DriverAdmin(admin.ModelAdmin):
     model = Driver
+class BuktiTayangAdmin(admin.ModelAdmin):
+    model = BuktiTayang
 
+admin.site.register(BuktiTayang, BuktiTayangAdmin)
 admin.site.register(Driver, DriverAdmin)
